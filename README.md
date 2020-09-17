11ce8f2492687146b4241514fd12baf96e08efb9
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-17 20:04:47 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JavaScript Hide and Seek
+ector# JavaScript Hide and Seek
 
 ## Objectives
 
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+
+function nestedTarget(){
+  return document.querySelector(".target");
+}
+
+function deepestChild(){
+  var grandNode = document.querySelector("#grand-node");
+  // var child = grandNode.children[0];
+  while(grandNode.children[0]){
+    grandNode=grandNode.children[0]
+    
+  }
+  return grandNode;
+}
+
+function increaseRankBy(n){
+var rankedList=document.querySelector('ul.ranked-list li');
+for(let i=0;i<rankedList.length;i++){
+  console.log("ranked-list: " + rankedList);
+}
+  
+}
