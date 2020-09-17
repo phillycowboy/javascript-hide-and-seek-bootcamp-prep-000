51874ab9bf04c693f5643d7f7ba920b321db90e2
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-17 20:26:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+
+function nestedTarget(){
+  return document.querySelector(".target");
+}
+
+function deepestChild(){
+  var grandNode = document.querySelector("#grand-node");
+  // var child = grandNode.children[0];
+  while(grandNode.children[0]){
+    grandNode=grandNode.children[0]
+    
+  }
+  return grandNode;
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
